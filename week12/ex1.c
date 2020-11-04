e3cb6ffd891dfe29d39519f4a2a9ecf25e4a1aff
akbarmadaminov <mr.Inovak@gmail.com>
2020-11-04 21:05:33 +0300

    Add files via upload

---
+++ week12/ex1.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define sz 20
+
+int main() {
+    char s[sz+1];
+    FILE *fp = fopen("/dev/random", "r");
+
+    fgets(s, sz+1, fp);
+    FILE *out = fopen("ex1.txt", "w");
+
+    fprintf(out, "%s", s);
+    printf("%s", s);
+    fclose(fp);
+    fclose(out);
+}
+
